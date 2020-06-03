af55c8dc3f41a8f8f83c2f4de72b5dc029aadae9
WKpeKepW <dan9o5ru@gmail.com>
2020-06-03 23:26:22 +0400

    First commit

---
+++ CDashE/CollectionEmotic.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Security.Policy;
+using System.Text;
+using System.Windows.Controls;
+
+namespace CDashE
+{
+    public static class CollectionEmotic
+    {
+        static string Text;
+        static string FinallyText;
+        static Dictionary<string, string> Emotic;
+        static void InitializeEmotic()//сделать конфиг для смайликов
+        {
+            var E = new Dictionary<string, string>();
+            StreamReader sr = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "\\Emotic.txt");
+            string T = sr.ReadToEnd();
+            sr.Close();
+            string[] buff = T.Split(';');
+            for (int i = 0; i < buff.Length; i++)
+            {
+                string tex = buff[i];
+                for (int l = 0; l < tex.Length; l++)
+                {
+                    if (tex[l] == '|')
+                        E.Add(tex.Remove(l), tex.Remove(0, l+1));
+                }
+            }
+            Emotic = E;
+        }
+        public static string ReturnEmotic(TextBox text)//ну нас будет один метод статичный в который и послыаеться тексбокс в статичный класс
+        {
+            Text = text.Text;
+            InitializeEmotic();
+            ReturnFormatText();
+            return SearchEmotic();
+        }
+        //<---этот метод будет переводить мои смайлики
+        static string SearchEmotic()
+        {
+            int i = 0;
+            FinallyText = null;
+            while (i != Text.Length)
+            {
+                string One = Text[i].ToString();
+                string Double;
+                if (Text.Length > i + 1)
+                    Double = Text[i].ToString() + Text[i + 1].ToString();
+                else
+                    Double = One;
+
+                foreach (var e in Emotic)
+                {
+                    string Key = e.Key.ToString();
+                    string Value = e.Value.ToString();
+                    if (One == Key || Double == Key)
+                    {
+                        FinallyText += Value;
+                        break;
+                    }
+                    else if (One == Value || Double == Value)
+                    { 
+                        FinallyText += Key;
+                        break;
+                    }
+                }
+                i++;
+            }
+            return FinallyText;
+        }
+        static void ReturnFormatText()//<--этот метод очищает метод от какашки и оставляет только смайлики
+        {
+            int length = Emotic.Count;
+            for (int i = 0;i < Text.Length; i++)
+            {
+                int error = 0;
+                foreach (var e in Emotic)
+                {
+                    string str = Text[i].ToString();
+                    string Key = e.Key.ToString();
+                    string Value = e.Value.ToString();
+                    if (str != Key)
+                    {
+                        if (str != Value)
+                        {
+                            if (i + 1 < Text.Length)
+                                str = Text[i].ToString() + Text[i + 1].ToString();
+                            if (str != Key)
+                            {
+                                if (str != Value)
+                                    error++;
+                                else { i++; break; }
+                            }
+                            else{ i++; break; }
+                        }
+                        else break;
+                    }
+                    else break;
+                }
+                if (error >= length)
+                {
+                    Text = Text.Remove(i,1);
+                    i--;
+                }
+            }
+        }
+    }
+}
---
+++ CDashE/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CDashE
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Bresult_Click(object sender, RoutedEventArgs e)// сделать хороший интерфейс который будет менятся 
+        {                                                           //динамичиски как только я буду делать его совсем мал
+            TForemoticBuff.Text = CollectionEmotic.ReturnEmotic(TForemotic);//а так же нужно будет сделать чтобы приложение
+                                                                            //само считывало буфер обмена и закидовало туда 
+        }
+
+        private void Past_Click(object sender, RoutedEventArgs e)
+        {
+            TForemotic.Text = Clipboard.GetText();
+        }
+
+        private void Copy_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(TForemoticBuff.Text);
+        }
+
+        private void ChangeConfig_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(AppDomain.CurrentDomain.BaseDirectory + "\\Emotic.txt") { UseShellExecute = true });
+        }
+    }
+}
